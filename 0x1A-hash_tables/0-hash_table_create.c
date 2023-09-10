31153f9369fde1739dffd830d22716f3f185832c
tabbytiropAI <tabbytirop@gmail.com>
2023-09-10 01:44:13 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,6 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function that creates a hash table
- * @size: size of the array
- * Return: the newly created hast table
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
@@ -13,32 +9,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_htable;
-	unsigned long int w; /*counter of the array*/
 	hash_table_t *ht;
 	unsigned long int i;
 
-	new_htable = (hash_table_t *)malloc(sizeof(hash_table_t));
-	if (new_htable == NULL)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	new_htable->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-	if (new_htable->array == NULL)
-	{
-		free(new_htable);
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	}
-	for (w = 0; w < size; w++)
-	{
-		new_htable->array[w] = NULL;
-	}
-	new_htable->size = size;
-	return (new_htable);
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
